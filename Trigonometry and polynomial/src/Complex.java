90effaf4aafad67b214793f4886b3fee621c3441
Arham Apon <arhamaponutsho@gmail.com>
2024-11-05 00:06:40 +0600

    add and subtract

--- Trigonometry and polynomial/src/Complex.java
+++ Trigonometry and polynomial/src/Complex.java
@@ -1,4 +1,18 @@
 public class Complex {
 
-    
+    double real;
+    double imag;
+
+    public Complex(double real, double imag) {
+        this.real = real;
+        this.imag = imag;
+    }
+
+    public Complex add(Complex other) {
+        return new Complex(this.real + other.real, this.imag + other.imag);
+    }
+
+    public Complex subtract(Complex other) {
+        return new Complex(this.real - other.real, this.imag - other.imag);
+    }
 }
