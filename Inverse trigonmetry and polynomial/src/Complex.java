0a467f07ac138e02da6b756f2caa816209911951
Arham Apon <arhamaponutsho@gmail.com>
2024-11-04 23:56:50 +0600

    complex multiply and divide

--- Inverse trigonmetry and polynomial/src/Complex.java
+++ Inverse trigonmetry and polynomial/src/Complex.java
@@ -16,6 +16,15 @@ public class Complex {
         return new Complex(this.real - other.real, this.imag - other.imag);
     }
 
-    
+    public Complex multiply(Complex other) {
+        return new Complex(this.real * other.real - this.imag * other.imag,
+                this.real * other.imag + this.imag * other.real);
+    }
+
+    public Complex divide(Complex other) {
+        double denominator = other.real * other.real + other.imag * other.imag;
+        return new Complex((this.real * other.real + this.imag * other.imag) / denominator,
+                (this.imag * other.real - this.real * other.imag) / denominator);
+    }
 
 }
