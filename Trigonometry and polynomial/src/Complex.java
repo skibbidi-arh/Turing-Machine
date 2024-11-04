b149cd62e4da5bd41bd1899c8736312d9e35dcbe
Arham Apon <arhamaponutsho@gmail.com>
2024-11-05 00:09:41 +0600

    complex for solving polynomials

--- Trigonometry and polynomial/src/Complex.java
+++ Trigonometry and polynomial/src/Complex.java
@@ -9,21 +9,42 @@ public class Complex {
     }
 
     public Complex add(Complex other) {
+
         return new Complex(this.real + other.real, this.imag + other.imag);
     }
 
     public Complex subtract(Complex other) {
+
         return new Complex(this.real - other.real, this.imag - other.imag);
+
     }
 
     public Complex multiply(Complex other) {
+
         return new Complex(this.real * other.real - this.imag * other.imag,
+
                 this.real * other.imag + this.imag * other.real);
     }
 
     public Complex divide(Complex other) {
         double denominator = other.real * other.real + other.imag * other.imag;
+
         return new Complex((this.real * other.real + this.imag * other.imag) / denominator,
                 (this.imag * other.real - this.real * other.imag) / denominator);
     }
+
+
+    public double magnitude() {
+
+        return Math.sqrt(real * real + imag * imag);
+    }
+
+
+    @Override
+    public String toString() {
+
+        return String.format("%.6f + %.6fi", real, imag);
+
+    }
+
 }
