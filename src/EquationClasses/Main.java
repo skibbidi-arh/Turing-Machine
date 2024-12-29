dbff55e56dc7f32fe3f4ca01a827010b88f55de6
Arham Apon <arhamaponutsho@gmail.com>
2024-12-29 22:40:27 +0600

    cleaned

--- src/EquationClasses/Main.java
+++ src/EquationClasses/Main.java
@@ -12,7 +12,7 @@ public class Main {
        // SolutionClass src = new SolutionClass();
 
         ModelApply ml = new ModelApply();
-        ml.LinearRG("homeprices.csv",5000);
+       // ml.LinearRG("homeprices.csv",5000);
 
 
     }
