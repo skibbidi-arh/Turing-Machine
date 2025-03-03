87b2ac2f79b191ffb6a8c5d48e65051771dfccc4
Golam6967 <coclans6982@gmail.com>
2025-03-03 11:37:21 +0600

    Changed the base result

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,5 @@ public class Main {
         Graph_Creator.plotdata(1, -1, 0);
         Graph_Creator.showgraph();
         System.out.println("This is has worked perfectly");
-        System.out.println("this is a test project");
     }
 }
