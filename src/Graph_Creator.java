3cb3c8a25895db863a90361ab3e7edbe2d0717fb
Golam6967 <coclans6982@gmail.com>
2025-03-03 10:53:30 +0600

    5th commit

---
+++ src/Graph_Creator.java
@@ -0,0 +1,53 @@
+public class Graph_Creator {
+    private static int n = 101;
+    private static char[][] arr;
+
+    public Graph_Creator() {
+        this.EmptyGraphCreator();
+    }
+
+    public void EmptyGraphCreator() {
+        for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < n; ++j) {
+                arr[i][j] = ' ';
+            }
+        }
+
+        this.createaxis();
+    }
+
+    public void createaxis() {
+        int i;
+        for(i = n - 1; i >= 0; --i) {
+            arr[50][i] = '.';
+        }
+
+        for(i = n - 1; i >= 0; --i) {
+            arr[i][50] = '.';
+        }
+
+    }
+
+    public static void showgraph() {
+        for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < n; ++j) {
+                System.out.print(arr[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+
+    }
+
+    public static void plotdata(int n_data, int m_data, int constant) {
+        for(int i = 6 + constant; i >= -6 - constant; --i) {
+            int x = (constant + m_data * i) / n_data;
+            arr[50 + i][50 + x] = '*';
+        }
+
+    }
+
+    static {
+        arr = new char[n][n];
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Graph_Creator gc = new Graph_Creator();
+        gc.EmptyGraphCreator();
+        Graph_Creator.plotdata(1, -1, 0);
+        Graph_Creator.showgraph();
+        System.out.println("This is has worked perfectly");
+    }
+}
