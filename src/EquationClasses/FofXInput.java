da94e190ef6156602ac553424c94106cee090bdf
Arham Apon <arhamaponutsho@gmail.com>
2025-03-12 02:55:25 +0600

    c

---
+++ src/EquationClasses/FofXInput.java
@@ -0,0 +1,139 @@
+package src.EquationClasses;
+
+import java.util.Scanner;
+public class FofXInput {
+    Scanner sc = new Scanner(System.in);
+
+    int[] x1 = new int[75];
+
+    void takex1(){
+
+
+        // For x^3 (indices 3 to 8)
+        System.out.println("Enter sin x^3:");
+        x1[3] = sc.nextInt();
+        System.out.println("Enter cos x^3:");
+        x1[4] = sc.nextInt();
+        System.out.println("Enter tan x^3:");
+        x1[5] = sc.nextInt();
+        System.out.println("Enter sec x^3:");
+        x1[6] = sc.nextInt();
+        System.out.println("Enter cosec x^3:");
+        x1[7] = sc.nextInt();
+        System.out.println("Enter cot x^3:");
+        x1[8] = sc.nextInt();
+
+        // For x^2 (indices 9 to 14)
+        System.out.println("Enter sin x^2:");
+        x1[9] = sc.nextInt();
+        System.out.println("Enter cos x^2:");
+        x1[10] = sc.nextInt();
+        System.out.println("Enter tan x^2:");
+        x1[11] = sc.nextInt();
+        System.out.println("Enter sec x^2:");
+        x1[12] = sc.nextInt();
+        System.out.println("Enter cosec x^2:");
+        x1[13] = sc.nextInt();
+        System.out.println("Enter cot x^2:");
+        x1[14] = sc.nextInt();
+
+        // For x (indices 15 to 20)
+        System.out.println("Enter sin x:");
+        x1[15] = sc.nextInt();
+        System.out.println("Enter cos x:");
+        x1[16] = sc.nextInt();
+        System.out.println("Enter tan x:");
+        x1[17] = sc.nextInt();
+        System.out.println("Enter sec x:");
+        x1[18] = sc.nextInt();
+        System.out.println("Enter cosec x:");
+        x1[19] = sc.nextInt();
+        System.out.println("Enter cot x:");
+        x1[20] = sc.nextInt();
+
+        // For x^3 inverse trigonometric functions (indices 30 to 35)
+        System.out.println("Enter arcsin x^3:");
+        x1[30] = sc.nextInt();
+        System.out.println("Enter arccos x^3:");
+        x1[31] = sc.nextInt();
+        System.out.println("Enter arctan x^3:");
+        x1[32] = sc.nextInt();
+        System.out.println("Enter arcsec x^3:");
+        x1[33] = sc.nextInt();
+        System.out.println("Enter arccosec x^3:");
+        x1[34] = sc.nextInt();
+        System.out.println("Enter arccot x^3:");
+        x1[35] = sc.nextInt();
+
+        // For x^2 inverse trigonometric functions (indices 36 to 41)
+        System.out.println("Enter arcsin x^2:");
+        x1[36] = sc.nextInt();
+        System.out.println("Enter arccos x^2:");
+        x1[37] = sc.nextInt();
+        System.out.println("Enter arctan x^2:");
+        x1[38] = sc.nextInt();
+        System.out.println("Enter arcsec x^2:");
+        x1[39] = sc.nextInt();
+        System.out.println("Enter arccosec x^2:");
+        x1[40] = sc.nextInt();
+        System.out.println("Enter arccot x^2:");
+        x1[41] = sc.nextInt();
+
+        // For x inverse trigonometric functions (indices 42 to 47)
+        System.out.println("Enter arcsin x:");
+        x1[42] = sc.nextInt();
+        System.out.println("Enter arccos x:");
+        x1[43] = sc.nextInt();
+        System.out.println("Enter arctan x:");
+        x1[44] = sc.nextInt();
+        System.out.println("Enter arcsec x:");
+        x1[45] = sc.nextInt();
+        System.out.println("Enter arccosec x:");
+        x1[46] = sc.nextInt();
+        System.out.println("Enter arccot x:");
+        x1[47] = sc.nextInt();
+
+
+
+        // Coefficients for exponential terms
+        System.out.println("Enter coefficient for exp(x^3):");
+        x1[27] = sc.nextInt();
+        System.out.println("Enter coefficient for exp(x^2):");
+        x1[28] = sc.nextInt();
+        System.out.println("Enter coefficient for exp(x):");
+        x1[29] = sc.nextInt();
+
+
+
+        System.out.println("Enter coefficient for exp(x^3):");
+        x1[24] = sc.nextInt();
+        System.out.println("Enter coefficient for exp(x^2):");
+        x1[25] = sc.nextInt();
+        System.out.println("Enter coefficient for exp(x):");
+        x1[26] = sc.nextInt();
+
+
+
+        // Coefficients for logarithmic and exponential terms
+        System.out.println("Enter coefficients for logarithmic and exponential terms:");
+        System.out.println("Enter coefficient for log(x):");
+        x1[23] = sc.nextInt();
+        System.out.println("Enter coefficient for log(x^2):");
+        x1[22] = sc.nextInt();
+        System.out.println("Enter coefficient for log(x^3):");
+        x1[21] = sc.nextInt();
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
