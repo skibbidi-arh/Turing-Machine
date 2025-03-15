01adf85a4701f6bca6a761b6476b7fc77e863cc2
Arham Apon <arhamaponutsho@gmail.com>
2025-03-16 03:52:24 +0600

    Quiz saving system

---
+++ src/SaveFiles/QuestionSave.java
@@ -0,0 +1,12 @@
+package SaveFiles;
+
+public class QuestionSave {
+
+    QuizNumber qn = new QuizNumber();
+    String number = Integer.toString(qn.getLatestQuizNumber());
+    String filename= "Quiz number " + number;
+
+
+
+
+}
--- src/Test/TestUtsho.java
+++ src/Test/TestUtsho.java
@@ -4,17 +4,33 @@ package Test;
 
 import EquationClasses.LogarithmCalculator;
 import Graph.TrigonometricGraph;
+import SaveFiles.QuizNumber;
+import SaveFiles.QuizWriter;
 
 public class TestUtsho {
 
     public static void main(String[] args) {
 
+        String quizContent = "Linear Algebra Quiz\n" +
+                "____________________\n\n" +
+                "Consider the matrix A as shown below.\n\n" +
+                "[-3  4  2]\n" +
+                "[ 6  3  1]\n" +
+                "[ 4 -7 -8]\n\n" +
+                "a). Find the inverse of the matrix\n" +
+                "b). Determinant of the matrix\n" +
+                "c). Determinant of the inverse.";
 
-        System.out.println("ln(2): " + EquationClasses.LogarithmCalculator.ln(2));
-        System.out.println("log10(100): " + LogarithmCalculator.ln(100));
-        System.out.println("logBase(5, 2.5): " + LogarithmCalculator.logBase(5, 2.4));
-        TrigonometricGraph graph = new TrigonometricGraph();
-        graph.draw();
+
+
+        QuizNumber quizNumber = new QuizNumber();
+        quizNumber.quizNumberUpdate();
+        int num = quizNumber.getLatestQuizNumber();
+        QuizWriter qr = new QuizWriter();
+        qr.WriteQuizQuestions(quizContent,num);
+        qr.readQuiz(num);
+
+
 
 
 
