8766bd47457557a0adeaa6fa1336d890b54a963d
Arham Apon <arhamaponutsho@gmail.com>
2025-03-15 17:08:25 +0600

    ok

--- src/Test/TestUtsho.java
+++ src/Test/TestUtsho.java
@@ -4,15 +4,17 @@ package Test;
 
 import EquationClasses.LogarithmCalculator;
 import Graph.TrigonometricGraph;
-import SaveFiles.StoreData;
 
 public class TestUtsho {
 
     public static void main(String[] args) {
 
 
-        StoreData storeData = new StoreData();
-        storeData.Store();
+        System.out.println("ln(2): " + EquationClasses.LogarithmCalculator.ln(2));
+        System.out.println("log10(100): " + LogarithmCalculator.ln(100));
+        System.out.println("logBase(5, 2.5): " + LogarithmCalculator.logBase(5, 2.4));
+        TrigonometricGraph graph = new TrigonometricGraph();
+        graph.draw();
 
 
 
