385343bec53deb120a51268ea5d2a24b77592180
Golam6967 <coclans6982@gmail.com>
2025-03-18 21:14:16 +0600

    before merging to ahir

--- src/Display/QuizDisplay.java
+++ src/Display/QuizDisplay.java
@@ -1,80 +1,104 @@
+
 package Display;
 
 import EquationClasses.Matrix;
 import EquationClasses.TrigonoValueError;
 import Interactive.*;
-import SaveFiles.QuestionSave;
 
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class QuizDisplay {
-    QuestionSave qs=new QuestionSave();
+
+    private static boolean timeUp = false;
+    private static int remainingTime = 20 * 60; // 20 minutes in seconds
 
     public QuizDisplay() {
     }
+
     public void display() throws TrigonoValueError {
         Scanner sc = new Scanner(System.in);
         System.out.println("Choose a Topic: ");
-        System.out.println("1.Linear Equation: ");
-        System.out.println("2.Polynomial Equation: ");
-        System.out.println("3.Vector: ");
-        int choice= sc.nextInt();
+        System.out.println("1. Linear Equation");
+        System.out.println("2. Polynomial Equation");
+        System.out.println("3. Vector");
+        int choice = sc.nextInt();
 
-        switch(choice) {
+        // Start 20-minute timer
+        startTimer();
+
+        switch (choice) {
             case 1:
-                System.out.println("Enter your name");
-                sc.nextLine();
-                String name = sc.nextLine();
-                String TotalData = "";
-                String topic = "Linear Algebra";
-                for (int i = 0; i < 3; i++) {
-                    PrintToFile pf = new PrintToFile();
-                    LinearQuestion lq = new LinearQuestion(i + 1, pf);
-                    lq.generateLinearQuestion();
-                    //pf.printdata(pf);
-                    TotalData += pf.getData();
-                    TotalData += "\n";
-                }
-                ResultMatcher rm = ResultMatcher.getMatcher();
-                System.out.println(rm.getMarks());
-                PrintToFile pf = new PrintToFile();
-                qs.save(name,rm.getMarks(),topic,TotalData);
-
-               // pf.writeStringToFile(TotalData, "D:/New folder/Turing-Machine/src/Interactive/Data.txt");
+                runLinearQuiz();
                 break;
             case 2:
-                String TotalData_3 = "";
-                for (int i = 0; i < 3; i++) {
-                    PrintToFile pf_3 = new PrintToFile();
-                    //LinearQuestion lq = new LinearQuestion(i + 1, pf_3);
-                    PolyNomialQuestion pq = new PolyNomialQuestion(i+1, pf_3);
-                    pq.GeneratePolynomialQuestion();
-                    //pf.printdata(pf);
-                    TotalData_3 += pf_3.getData();
-                    TotalData_3 += "\n";
-                }
-                ResultMatcher rm_3 = ResultMatcher.getMatcher();
-                System.out.println(rm_3.getMarks());
-                PrintToFile pf_3 = new PrintToFile();
-                pf_3.writeStringToFile(TotalData_3, "D:/New folder/Turing-Machine/src/Interactive/Data.txt");
+                runPolynomialQuiz();
                 break;
             case 3:
-                String TotalData_2 = "";
-                for (int i = 0; i < 3; i++) {
-                    PrintToFile pf_2 = new PrintToFile();
-                    VectorQuestion vq = new VectorQuestion(pf_2, i + 1);
-                    vq.GenerateOption();
-                    TotalData_2 += pf_2.getData();
-                    TotalData_2 += "\n";
-                }
-
-                PrintToFile pf_2 = new PrintToFile();
-                pf_2.writeStringToFile(TotalData_2, "D:/New folder/Turing-Machine/src/Interactive/Data.txt");
-                ResultMatcher rm_2 = ResultMatcher.getMatcher();
-                System.out.println("Marks: " + rm_2.getMarks());
+                runVectorQuiz();
                 break;
+            default:
+                System.out.println("Invalid choice. Please select a valid option.");
         }
-
     }
 
+    private void startTimer() {
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if (remainingTime <= 0) {
+                    timeUp = true;
+                    System.out.println("\nTime's up! The quiz has ended.");
+                    System.exit(0); // End the program when time is up
+                } else {
+                    remainingTime--;
+                }
+            }
+        }, 0, 1000); // Update every second
+    }
+
+    private void runLinearQuiz() {
+        StringBuilder totalData = new StringBuilder();
+        for (int i = 0; i < 3 && !timeUp; i++) {
+            PrintToFile pf = new PrintToFile();
+            LinearQuestion lq = new LinearQuestion(i + 1, pf);
+            lq.generateLinearQuestion();
+            System.out.println("Remaining Time: " + (remainingTime / 60) + " min " + (remainingTime % 60) + " sec");
+            totalData.append(pf.getData()).append("\n");
+        }
+        saveResults(totalData.toString());
+    }
+
+    private void runPolynomialQuiz() {
+        StringBuilder totalData = new StringBuilder();
+        for (int i = 0; i < 3 && !timeUp; i++) {
+            PrintToFile pf = new PrintToFile();
+            PolyNomialQuestion pq = new PolyNomialQuestion(i + 1, pf);
+            pq.GeneratePolynomialQuestion();
+            System.out.println("Remaining Time: " + (remainingTime / 60) + " min " + (remainingTime % 60) + " sec");
+            totalData.append(pf.getData()).append("\n");
+        }
+        saveResults(totalData.toString());
+    }
+
+    private void runVectorQuiz() throws TrigonoValueError {
+        StringBuilder totalData = new StringBuilder();
+        for (int i = 0; i < 3 && !timeUp; i++) {
+            PrintToFile pf = new PrintToFile();
+            VectorQuestion vq = new VectorQuestion(pf, i + 1);
+            vq.GenerateOption();
+            System.out.println("Remaining Time: " + (remainingTime / 60) + " min " + (remainingTime % 60) + " sec");
+            totalData.append(pf.getData()).append("\n");
+        }
+        saveResults(totalData.toString());
+    }
+
+    private void saveResults(String data) {
+        PrintToFile pf = new PrintToFile();
+        pf.writeStringToFile(data, "D:/New folder/Turing-Machine/src/Interactive/Data.txt");
+        ResultMatcher rm = ResultMatcher.getMatcher();
+        System.out.println("Marks: " + rm.getMarks());
+    }
 }
