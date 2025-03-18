0274cae8ee111b4ed9d241e0f63fadc80b347b97
Arham Apon <arhamaponutsho@gmail.com>
2025-03-18 16:00:39 +0600

    kkkkk

--- src/Test/TestUtsho.java
+++ src/Test/TestUtsho.java
@@ -3,37 +3,20 @@ package Test;
 
 
 import EquationClasses.LogarithmCalculator;
+import EquationClasses.TrigonoValueError;
 import Graph.TrigonometricGraph;
 import SaveFiles.QuestionSave;
 import SaveFiles.QuizNumber;
 import SaveFiles.QuizWriter;
 import SaveFiles.StoreData;
+import Display.*;
 
 public class TestUtsho {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TrigonoValueError {
 
-        String quizContent = "Linear Algebra Quiz\n" +
-                "____________________\n\n" +
-                "Consider the matrix A as shown below.\n\n" +
-                "[-3  4  2]\n" +
-                "[ 6  3  1]\n" +
-                "[ 4 -7 -8]\n\n" +
-                "a). Find the inverse of the matrix\n" +
-                "b). Determinant of the matrix\n" +
-                "c). Determinant of the inverse.";
-
-
-        QuestionSave qs= new QuestionSave();
-
-
-        StoreData sd= new StoreData();
-       // sd.searchRecords("Ahir");
-        sd.showLeaderboard("Linear Algebra");
-
-
-
-
+        QuizDisplay display = new QuizDisplay();
+        display.display();
 
 
     }
