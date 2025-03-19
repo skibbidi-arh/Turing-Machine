8632fa00b3acec7e6f59814c2dfc778457c28405
Golam6967 <coclans6982@gmail.com>
2025-03-19 07:32:20 +0600

    Done

--- src/Main.java
+++ src/Main.java
@@ -10,23 +10,6 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) throws TrigonoValueError {
-//        Display display = new Display();
-//        display.display();
-//        VectorQuestion vq = new VectorQuestion();
-//        Random rand = new Random();
-//        vq.GenerateOption();
-//        Matrix_Gen m= new Matrix_Gen(3);
-//        m.array_initialization();
-//
-//        Matrix matrix = new Matrix(m.getVariable_matrix());
-//        matrix.printMatrix();
-//
-//        m.array_initialization();
-//        Matrix matrix2 = new Matrix(m.getVariable_matrix());
-//        matrix2.printMatrix();
-//        LinearQuestion lq = new LinearQuestion();
-//        lq.generateLinearQuestion();
-
         Display display = new Display();
         display.display();
 
